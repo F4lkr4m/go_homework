5814ece53dc491cbd2c4b95d9ddfbe1f067ac33a
Sergey <falkr4m@yandex.ru>
2021-09-05 20:02:31 +0200

    writing input transformation

--- calc/calc.go
+++ calc/calc.go
@@ -1,8 +1,34 @@
 package calc
 
+import (
+	"strings"
+)
+
 type Calc struct {}
 
-func (calc *Calc) Solve(input string) (result float64, err error) {
+func (calc *Calc) Solve(input string) (result float64, outErr error) {
+	input = strings.Replace(input, " ", "", -1)
+
+
+	defer func() {
+		if r := recover(); r != nil {
+			outErr = r.(error)
+		}
+	}()
+	return
+}
+
+func splitToOperands(input string) (output []string, err error) {
+	for i := 0; i < len(input); {
+
+	}
+	return 
+}
+
+func getNextOperand(input string, position int) (output string, nextSignIndex int, err error) {
+	if position >= len(input) {
+		panic("Out of index of input line")
+	}
 
 	return
 }
