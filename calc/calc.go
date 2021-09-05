5f905dbbb8d5c24585c1add58463d4c5e878f252
Sergey <falkr4m@yandex.ru>
2021-09-05 19:43:31 +0200

    written tests to calc

---
+++ calc/calc.go
@@ -0,0 +1,13 @@
+package calc
+
+type Calc struct {}
+
+func (calc *Calc) Solve(input string) (result float64, err error) {
+
+	return
+}
+
+func convertToPolishSystem(input []string) (result []string, err error) {
+
+	return
+}
---
+++ calc/calc_test.go
@@ -0,0 +1,117 @@
+package calc
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+var sumTests = []struct {
+	in string
+	out float64
+} {
+	{"1 + 3", 4},
+	{"1 + 7", 8},
+	{"1.5 + 3.5", 5.0},
+	{"1.3 + 2.3", 3.6},
+	{"1000000 + 1", 1000001},
+}
+
+var subtractionTests = []struct {
+	in string
+	out float64
+} {
+	{"1 - 3", -2},
+	{"1 - 7", -6},
+	{"3.5 - 1.5", 2.0},
+	{"1.3 - 2.3", -1.0},
+	{"1000000 - 1", 999999},
+}
+
+var divTests = []struct {
+	in string
+	out float64
+} {
+	{"4 / 2", 2},
+	{"9 / 3", 3},
+	{"10 / 5", 2.0},
+	{"9.3 / 3", 3.1},
+}
+
+var mulTests = []struct {
+	in string
+	out float64
+} {
+	{"1 * 3", 3},
+	{"2 * 7", 14},
+	{"1.5 * 3.5", 5.25},
+	{"9999 * 999", 9989001},
+}
+
+var tests = [][]struct{
+	in string
+	out float64} {
+	sumTests,
+	subtractionTests,
+	divTests,
+	mulTests,
+}
+
+func TestElementaryOperations(t *testing.T) {
+	calc := Calc{}
+	for _, cases := range tests {
+		for _, tt := range cases {
+			t.Run(tt.in, func(t *testing.T) {
+				val, err := calc.Solve(tt.in)
+				if err != nil {
+					t.Errorf("Something went wrong, error in solving elementary operations.")
+				}
+				require.Equal(t, tt.out, val, "Two numbers should be the same.")
+			})
+		}
+	}
+}
+
+var hardTests = []struct {
+	in string
+	out float64
+} {
+	{"1 + 3 + (4 * (3 + (4 / 4)))", 20},
+	{"1 - 4 * (3 + 4)", -27},
+	{"2 * (3 + 4 * 4 * 5 * 5) * 3", 2418},
+	{"3 + 2 * 4 * 2 / (1 - 3)", -5},
+}
+
+func TestHardOperations(t *testing.T) {
+	calc := Calc{}
+	for _, tt := range hardTests {
+		t.Run(tt.in, func(t *testing.T) {
+			val, err := calc.Solve(tt.in)
+			if err != nil {
+				t.Errorf("Something went wrong, error in solving elementary operations.")
+			}
+			require.Equal(t, tt.out, val, "Two numbers should be the same.")
+		})
+	}
+}
+
+var errorTests = []struct {
+	in string
+	out string
+} {
+	{"1 + 3 + (4 * (3 + (4 / 1))", "Parsing expression error"},
+	{"1.2 + 3 + (4 * (3 + (4 / 1)))", "Parsing expression error"},
+	{"Hello there", "Parsing expression error"},
+	{"1 + 3 + (4 * (3 + (4 / 0)))", "Zero division"},
+	{"1 + 3 + (4 * (3 + (4 / (1 - 1))))", "Zero division"},
+}
+
+
+func TestErrorInput(t *testing.T) {
+	calc := Calc{}
+	for _, tt := range errorTests {
+		t.Run(tt.in, func(t *testing.T) {
+			_, err := calc.Solve(tt.in)
+			require.EqualError(t, err, tt.out)
+		})
+	}
+}
