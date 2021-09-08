171d75facaf8bcc2f17fd824435b4edc1e0579dd
Sergey <falkr4m@yandex.ru>
2021-09-09 00:24:01 +0300

    added standard logic

--- uniq/uniq.go
+++ uniq/uniq.go
@@ -1,5 +1,7 @@
 package uniq
 
+import "strings"
+
 type Mode int
 
 const (
@@ -13,11 +15,81 @@ const FileNum = 2
 
 type Options struct {
 	I bool  // no case-sensitive
-
-	WorkMode Mode
 	SChars int
 	FFields int
 
+	WorkMode Mode
+
 	InputFilename string
 	OutputFilename string
+}
+
+func FormatLinesWithOptions(lines []string, i bool, SChars int, FFields int) (out []string) {
+	Outer:
+		for _, line := range lines {
+			formattedLine := line
+
+			for i := 0; i < FFields; i++ {
+				if index := strings.IndexByte(line, ' '); index == -1 {
+					formattedLine = ""
+					out = append(out, "")
+					continue Outer
+				} else {
+					formattedLine = line[index:]
+				}
+			}
+
+			if len(formattedLine) > SChars {
+				formattedLine = formattedLine[SChars:]
+			} else {
+				formattedLine = ""
+				out = append(out, "")
+				continue Outer
+			}
+
+			if i {
+				formattedLine = strings.ToLower(formattedLine)
+			}
+
+			out = append(out, formattedLine)
+		}
+	return out
+}
+
+func standardLogic(lines []string, formattedLines []string) (out []string){
+	// just add the first one line
+	out = append(out, lines[0])
+
+	// check other lines
+	for i := 1; i < len(lines) - 1; i++ {
+		if formattedLines[i] != formattedLines[i - 1] {
+			out = append(out, lines[i])
+		}
+	}
+
+	if formattedLines[len(lines) - 1] != formattedLines[len(lines) - 2] {
+		out = append(out, lines[len(lines) - 1])
+	}
+
+	return out
+}
+
+func Uniq(lines []string, opt Options) (out []string) {
+	//originalLines := lines
+
+	formattedLines := FormatLinesWithOptions(lines, opt.I, opt.SChars, opt.FFields)
+
+	switch opt.WorkMode {
+	case None:
+		return standardLogic(lines, formattedLines)
+	case C:
+
+	case U:
+
+	case D:
+
+	}
+
+
+	return
 }
--- uniq/uniq_test.go
+++ uniq/uniq_test.go
@@ -1,1 +1,181 @@
 package uniq
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+var formattingTest = []struct {
+	i       bool
+	fFields int
+	sChars  int
+	in      []string
+	out     []string
+}{
+	{ i: true,
+		fFields: 0,
+		sChars: 0,
+		in: []string{"HelloEverybody", "Hell"},
+		out: []string{"helloeverybody", "hell"},
+	},
+	{ i: true,
+		fFields: 0,
+		sChars: 0,
+		in: []string{"HelloEverybody", "Hell", "CMON", "HoWArEY o U"},
+		out: []string{"helloeverybody", "hell", "cmon", "howarey o u"},
+	},
+	{ i: true,
+		fFields: 0,
+		sChars: 3,
+		in: []string{"HelloEverybody", "Hell", "CMON", "HoWArEY o U"},
+		out: []string{"loeverybody", "l", "n", "arey o u"},
+	},
+	{ i: true,
+		fFields: 0,
+		sChars: 5,
+		in: []string{"HelloEverybody", "Hell", "CMON", "HoWArEY o U"},
+		out: []string{"everybody", "", "", "ey o u"},
+	},
+	{ i: false,
+		fFields: 1,
+		sChars: 4,
+		in: []string{
+			"We 1ove music.",
+			"I lo1e music.",
+			"They l1ve music.",
+		},
+		out: []string{"e music.", "e music.", "e music."},
+	},
+}
+
+func TestFormattingLinesWithOptions(t *testing.T) {
+	for _, tt := range formattingTest {
+		t.Run(tt.in[0], func(t *testing.T) {
+			result := FormatLinesWithOptions(tt.in, tt.i, tt.sChars, tt.fFields)
+			if len(result) != len(tt.out) {
+				t.Fatal("Arrays not equal length")
+			}
+
+			for i := 0; i < len(result); i++ {
+				require.Equal(t, tt.out[i], result[i], "Cell in array %d", i)
+			}
+		})
+	}
+}
+
+var stdTest = []struct {
+	i       bool
+	fFields int
+	sChars  int
+	in      []string
+	out     []string
+}{
+	{
+		i: false,
+		fFields: 0,
+		sChars: 0,
+		in: []string {
+			"I love music.",
+			"I love music.",
+			"I love music.",
+			"",
+			"I love music of Kartik.",
+			"I love music of Kartik.",
+			"Thanks.",
+			"I love music of Kartik.",
+			"I love music of Kartik.",
+		},
+		out: []string {
+			"I love music.",
+			"",
+			"I love music of Kartik.",
+			"Thanks.",
+			"I love music of Kartik.",
+		},
+	},
+	{
+		i: true,
+		fFields: 0,
+		sChars: 0,
+		in: []string {
+			"I LOVE MUSIC.",
+			"I love music.",
+			"I LoVe MuSiC.",
+			"",
+			"I love MuSIC of Kartik.",
+			"I love music of Kartik.",
+			"Thanks.",
+			"I love music of Kartik.",
+			"I love MuSIC of Kartik.",
+		},
+		out: []string {
+			"I LOVE MUSIC.",
+			"",
+			"I love MuSIC of Kartik.",
+			"Thanks.",
+			"I love music of Kartik.",
+		},
+	},
+	{
+		i: false,
+		fFields: 1,
+		sChars: 0,
+		in: []string {
+			"We love music.",
+			"I love music.",
+			"They love music.",
+			"",
+			"I love music of Kartik.",
+			"We love music of Kartik.",
+			"Thanks.",
+		},
+		out: []string {
+			"We love music.",
+			"",
+			"I love music of Kartik.",
+			"Thanks.",
+		},
+	},
+	{
+		i: false,
+		fFields: 0,
+		sChars: 1,
+		in: []string {
+			"I love music.",
+			"A love music.",
+			"C love music.",
+			"",
+			"I love music of Kartik.",
+			"We love music of Kartik.",
+			"Thanks.",
+		},
+		out: []string {
+			"I love music.",
+			"",
+			"I love music of Kartik.",
+			"We love music of Kartik.",
+			"Thanks.",
+		},
+	},
+
+}
+
+func TestUniqStandard(t *testing.T) {
+	for _, tt := range stdTest {
+		t.Run(tt.in[0], func(t *testing.T) {
+			opt := Options{WorkMode: 0,
+				I: tt.i,
+				FFields: tt.fFields,
+				SChars: tt.sChars,
+			}
+			result := Uniq(tt.in, opt)
+			if len(result) != len(tt.out) {
+				t.Fatalf("Arrays not equal length result: %d out: %d", len(result), len(tt.out))
+			}
+
+			for i := 0; i < len(result); i++ {
+				require.Equal(t, tt.out[i], result[i], "Cell in array %d", i)
+			}
+		})
+	}
+}
